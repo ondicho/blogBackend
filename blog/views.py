bb49be7fa0b5eb879c4d743f5f7f2959c1b408c1
ondicho <ondicho53@gmail.com>
2022-03-06 00:11:12 +0300

    write view for get method to retreive posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
+import imp
+import re
 from django.shortcuts import render
-
+from blog.models import Post
+from blog.serializers import PostSerializer
+from rest_framework.response import Response
 # Create your views here.
+
+
+def blogView(request):
+    if request.method=='GET':
+        queryset=Post.objects.all()
+        posts=PostSerializer(queryset)
+
+        return Response(posts)
