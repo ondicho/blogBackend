1f80ecdd465d1db9ff45238aad4fda9a68411b56
ondicho <ondicho53@gmail.com>
2022-03-07 00:04:54 +0300

    create a post method for our blog view

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,20 @@ from django.shortcuts import render
 from blog.models import Post
 from blog.serializers import PostSerializer
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
-
+@api_view(['GET','POST'])
 def blogView(request):
     if request.method=='GET':
         queryset=Post.objects.all()
-        posts=PostSerializer(queryset)
+        posts=PostSerializer(queryset,many=True)
 
-        return Response(posts)
+        return Response(posts.data)
+    elif(request.method == 'POST'):
+        serializers = PostSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
